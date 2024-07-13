17b53ec16c1d66d1edd960f190b3e1cc68355372
giftedpromise <giftedpromise1992@gmail.com>
2024-07-13 20:59:58 +0100

    api implementation

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://timbu-get-all-products.reavdev.workers.dev",
+        target: "https://timbu-get-all-products.reavdev.workers.dev/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
