36a1dece295fe0642606247cc65563a833258aae
giftedpromise <giftedpromise1992@gmail.com>
2024-07-14 21:22:01 +0100

    api implementation

--- src/data/data.js
+++ src/data/data.js
@@ -2,16 +2,19 @@ import axios from "axios";
 
 export const getProducts = async (currentPage) => {
   try {
-    const response = await axios.get("/api/products", {
-      params: {
-        organization_id: "aae3d11b193e4344b430f0177b4295d3",
-        reverse_sort: false,
-        page: currentPage,
-        size: 10,
-        Appid: "LXVE8ERZISOKH93",
-        Apikey: "8fc2f946109d4ee9960b79daed74864520240713011719071900",
-      },
-    });
+    const response = await axios.get(
+      " https://timbu-get-all-products.reavdev.workers.dev/ ",
+      {
+        params: {
+          organization_id: "aae3d11b193e4344b430f0177b4295d3",
+          reverse_sort: false,
+          page: currentPage,
+          size: 10,
+          Appid: "LXVE8ERZISOKH93",
+          Apikey: "8fc2f946109d4ee9960b79daed74864520240713011719071900",
+        },
+      }
+    );
     return response.data;
   } catch (error) {
     console.error("Failed to fetch products:", error);
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://api.timbu.cloud",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+  plugins: [react()],
 });
